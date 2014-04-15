9fda1ac823dfbaf5cb2d6bb761a3f5f0007d37cc
juan2ramos <juan2ramos@gmail.com>
2014-04-14 21:54:09 -0500

    arreglo inicio

--- app/views/front/includes/nav.blade.php
+++ app/views/front/includes/nav.blade.php
@@ -2,6 +2,7 @@
                     <?php
                     $menu = Menu::where('activo', '=', '1 order by id DESC')->get();
                     $pieces = explode('/', Request::path());
+                    $sub = '';
                     if(!empty($pieces[0])){
                         $sub = ($pieces[0] == 'vota-por-una-idea' || $pieces[0] == 'detalle-idea')?'categorias':false;
                     }
