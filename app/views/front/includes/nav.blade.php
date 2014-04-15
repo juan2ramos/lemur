7cb768aba2d05d212114def21fe4087c88551285
juan2ramos <juan2ramos@gmail.com>
2014-04-14 21:49:27 -0500

    activo link vota por una idea

--- app/views/front/includes/nav.blade.php
+++ app/views/front/includes/nav.blade.php
@@ -1,10 +1,17 @@
 				<ul>
                     <?php
                     $menu = Menu::where('activo', '=', '1 order by id DESC')->get();
-                    
+                    $pieces = explode('/', Request::path());
+                    if(!empty($pieces[0])){
+                        $sub = ($pieces[0] == 'vota-por-una-idea' || $pieces[0] == 'detalle-idea')?'categorias':false;
+                    }
                     ?>
                     @foreach($menu as $link)
-                    <?php $current = (Request::path() == $link->url)?'current':''?>
+                    <?php
+                        $current = (Request::path() == $link->url)?'current':'';
+                        $current = ($sub == $link->url)?'current':'';
+
+                    ?>
                     <li class="{{$link->id_html}}-class {{$current}}">
 
                         {{ HTML::decode(HTML::link($link->url, '<p>' .$link->texto. '</p>', array('id' => $link->id_html))) }}
