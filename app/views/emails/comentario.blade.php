bbe675104ef18ab11d07385b272f9aba480fe74f
juan2ramos <juan2ramos@gmail.com>
2014-04-18 17:25:39 -0500

    + email comentarios + paginación + imagen idea

---
+++ app/views/emails/comentario.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+    {{HTML::image('images/lemur.png')}}
+		<h2>Bienvenido a Lemur</h2>
+         <p>Para finalizar tu registro da click {{HTML::link('finalizarRegistro/'.$key)}} e ingresa. </p>
+	</body>
+</html>
