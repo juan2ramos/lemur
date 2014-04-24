405f74e53db3757a4feefdc18fdc2c6d9a612fd0
juan2ramos <juan2ramos@gmail.com>
2014-04-24 17:07:35 -0500

    no message

--- app/views/layouts/layout.blade.php
+++ app/views/layouts/layout.blade.php
@@ -101,26 +101,6 @@
     <div id="blockG_3" class="facebook_blockG">
     </div>
 </div>
-<!-- JavaScript -->
-<?php echo HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js') ?>
-<?php echo HTML::script('js/jquery.flexslider-min.js') ?>
-<?php echo HTML::script('js/skrollr.min.js') ?>
-<?php echo HTML::script('js/script.js') ?>
-<?php echo HTML::script('js/jquery.mCustomScrollbar.concat.min.js') ?>
-@yield('javascript')
-@if (!Auth::check())
 
-
-{{HTML::script('https://apis.google.com/js/client:plusone.js')}}
-@endif
-
-
-<script>
-    $("#contend").mCustomScrollbar({
-        scrollButtons: {
-            enable: true
-        }
-    });
-</script>
 </body>
 </html>
